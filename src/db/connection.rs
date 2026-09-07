4e1a638a01ff700a9bbf983aad0180d7d53dbfe2
kunal171 <0xshadyr6@gmail.com>
2026-09-07 20:56:44 +0530

    feat: add user role column to users table and update database connection handling

--- src/db/connection.rs
+++ src/db/connection.rs
@@ -1,12 +1,13 @@
-use dotenvy::dotenv;
 use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+use std::time::Duration;
 
 pub async fn connect_db() -> Result<Pool<Postgres>, sqlx::Error> {
-    dotenv().ok();
+    dotenvy::dotenv().ok();
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
     let pool = PgPoolOptions::new()
         .max_connections(5)
+        .acquire_timeout(Duration::from_secs(3))
         .connect(&database_url)
         .await?;
 
